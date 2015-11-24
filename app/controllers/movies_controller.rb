d83ec0d186f3bb004fd4359ed24f21011189f85c
an1tahung <anitalhung@gmail.com>
2015-11-24 03:28:50 +0000

    Added flash notices.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,7 +20,7 @@ class MoviesController < ApplicationController
     @movie = Movie.new(movie_params)
 
     if @movie.save
-      redirect_to movies_url
+      redirect_to movies_url, notice: "#{@movie.title} was submitted successfully!"
     else
       render :new
     end
@@ -45,7 +45,7 @@ class MoviesController < ApplicationController
   protected
 
   def movie_params
-    params.requite(:movie).permit(
+    params.require(:movie).permit(
       :title, :release_date, :director, :runtime_in_minute, :poster_image_url, :description  
     )
   end
