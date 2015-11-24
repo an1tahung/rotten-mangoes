a7587e014c97ea155954d9721ecb7bc2d3574ab8
an1tahung <anitalhung@gmail.com>
2015-11-24 02:02:34 +0000

    Generated movies controller and views.

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,52 @@
+class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movies_url
+    else
+      render :new
+    end
+  end
+
+  def update 
+    @movie = Movie.find(params[:id])
+
+    if @movie.update_attributes(movie_params)
+      redirect_to movie_path(@movie)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+  protected
+
+  def movie_params
+    params.requite(:movie).permit(
+      :title, :release_date, :director, :runtime_in_minute, :poster_image_url, :description  
+    )
+  end
+end
