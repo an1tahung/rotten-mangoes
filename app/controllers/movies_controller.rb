e747757980b22dfad691e1ed23343a537b8b4615
an1tahung <anitalhung@gmail.com>
2015-11-24 04:08:45 +0000

    Fixed spelling error in movies controller.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,7 +20,7 @@ class MoviesController < ApplicationController
     @movie = Movie.new(movie_params)
 
     if @movie.save
-      redirect_to movies_url, notice: "#{@movie.title} was submitted successfully!"
+      redirect_to movies_path, notice: "#{@movie.title} was submitted successfully!"
     else
       render :new
     end
@@ -46,7 +46,7 @@ class MoviesController < ApplicationController
 
   def movie_params
     params.require(:movie).permit(
-      :title, :release_date, :director, :runtime_in_minute, :poster_image_url, :description  
+      :title, :release_date, :director, :runtime_in_minutes, :poster_image_url, :description  
     )
   end
 end
